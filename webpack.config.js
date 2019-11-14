26685cf2c9a5dcf66ef500e55b14c958e2d6da83
boikle <boikle@users.noreply.github.com>
2019-11-13 20:59:01 -0500

    Setup for webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-path = require('path');
+const path = require('path');
 
 module.exports = {
 	mode: 'production',
 	entry: './src/index.js',
-  	output: {
-    	path: path.resolve(__dirname, '../dist'),
-    	filename: 'simple-carousel.js'
-  	}
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'simple-carousel.js'
+	}
 };
