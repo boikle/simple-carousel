4c5b26fe6c390ff623c074971d6f8f90d485c033
Robert Oikle <6431161+roikle@users.noreply.github.com>
2019-11-19 05:24:28 -0500

    added getter functions for selecting slide elements

--- src/generator.js
+++ src/generator.js
@@ -1,10 +1,32 @@
-let getContainer = function() {
+let getSlides = function() {
 	const containerId = 'simple-carousel';
 	let carousel = document.getElementById(containerId);
-	carousel.textContent = "Simple Carousel";
+	let slides = carousel.getElementsByClassName('slide');
+	return slides;
+};
+
+let deactivateSlide = function(slide) {
+	slide.classList.remove('active');
+};
+
+let activateSlide = function(slide) {
+	slide.classList.add('active');
+};
+
+let rotateSlides = function() {
+
+	let slides = getSlides();
+
+	console.log(slides);
+//	slides.forEach(function(slide) {
+//		window.setTimeout(function() {
+//			activateSlide(slide);
+//		}, 5000);
+//		deactivateSlide(slide);
+//	});
 };
 
 export function addCarousel() {
-	getContainer();
+	rotateSlides();
 }
 
