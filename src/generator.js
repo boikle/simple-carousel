6998832314cfbbf27f100b657bfb34670d5583c3
Robert Oikle <6431161+roikle@users.noreply.github.com>
2019-11-19 20:02:05 -0500

    Added functions to set and get active slides

--- src/generator.js
+++ src/generator.js
@@ -1,3 +1,5 @@
+let currentSlide;
+
 let getSlides = function() {
 	const containerId = 'simple-carousel';
 	let carousel = document.getElementById(containerId);
@@ -5,8 +7,50 @@ let getSlides = function() {
 	return slides;
 };
 
-let deactivateSlide = function(slide) {
-	slide.classList.remove('active');
+let getActiveSlides = function() {
+	const containerId = 'simple-carousel';
+	let carousel = document.getElementById(containerId);
+	let activeSlides = carousel.getElementsByClassName('active');
+	return activeSlides;
+};
+
+let setCurrentSlide = function(index) {
+	currentSlide = index;
+};
+
+let getCurrentSlide = function() {
+	let i, e, slide;
+	let slides = getSlides();
+
+	if (!currentSlide && currentSlide !== 0) {
+		for (i = 0, e = slides.length; i < e; i += 1) {
+			slide = slides[i];
+			if (slide.classList.contains('active')) {
+				setCurrentSlide(i);
+			}
+		}
+	}
+
+	return currentSlide;
+};
+
+let incrementCurrentSlide = function() {
+	let slideIndex = getCurrentSlide();
+	if ((slideIndex + 1) < getSlides().length) {
+		setCurrentSlide(slideIndex + 1);
+	} else {
+		setCurrentSlide(0);
+	}
+};
+
+let deactivateSlides = function() {
+	let i, e, slide;
+	let activeSlides = getActiveSlides();
+
+	for (i = 0, e = activeSlides.length; i < e; i += 1) {
+		slide = activeSlides[i];
+		slide.classList.remove('active');
+	}
 };
 
 let activateSlide = function(slide) {
@@ -14,16 +58,14 @@ let activateSlide = function(slide) {
 };
 
 let rotateSlides = function() {
-
 	let slides = getSlides();
 
-	console.log(slides);
-//	slides.forEach(function(slide) {
-//		window.setTimeout(function() {
-//			activateSlide(slide);
-//		}, 5000);
-//		deactivateSlide(slide);
-//	});
+	window.setInterval(function() {
+		incrementCurrentSlide();
+		deactivateSlides();
+		let slide = slides[getCurrentSlide()];
+		activateSlide(slide);
+	}, 2000);
 };
 
 export function addCarousel() {
