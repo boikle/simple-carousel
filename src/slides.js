a434a6a35b3eaf89069127f48a77b788b7f2315d
Robert Oikle <6431161+boikle@users.noreply.github.com>
2019-11-23 20:32:16 -0500

    Converted slide code to a class

--- src/slides.js
+++ src/slides.js
@@ -1,38 +1,39 @@
-
-export let Slides = function() {
-	this.activeSlideIndex = 0;
-};
-
-Slides.prototype.getActiveSlide = function() {
-	return this.activeSlideIndex;
-};
-
-Slides.prototype.setActiveSlide = function(index) {
-	this.activeSlideIndex = index;
-};
-
-Slides.prototype.getSlides = function() {
-	const containerId = 'simple-carousel';
-	let carousel = document.getElementById(containerId);
-	let slides = carousel.getElementsByClassName('slide');
-	return slides;
-};
-
-Slides.prototype.incrementCurrentSlide = function() {
-	let startIndex = 0;
-	let slideIndex = this.getActiveSlide();
-	if ((slideIndex + 1) < this.getSlides().length) {
-		this.setActiveSlide(slideIndex + 1);
-	} else {
-		this.setActiveSlide(startIndex);
+export class Slides {
+	constructor() {
+		this.activeSlideIndex = 0;
 	}
-};
 
-Slides.prototype.deactivateSlide = function(slide) {
-	slide.classList.remove('active');
-};
+	getActiveSlide() {
+		return this.activeSlideIndex;
+	}
 
-Slides.prototype.activateSlide = function(slide) {
-	slide.classList.add('active');
-};
+	setActiveSlide(index) {
+		this.activeSlideIndex = index;
+	}
+
+	getSlides() {
+		const containerId = 'simple-carousel';
+		let carousel = document.getElementById(containerId);
+		let slides = carousel.getElementsByClassName('slide');
+		return slides;
+	}
+
+	incrementCurrentSlide() {
+		let startIndex = 0;
+		let slideIndex = this.getActiveSlide();
+		if ((slideIndex + 1) < this.getSlides().length) {
+			this.setActiveSlide(slideIndex + 1);
+		} else {
+			this.setActiveSlide(startIndex);
+		}
+	}
+
+	deactivateSlide(slide) {
+		slide.classList.remove('active');
+	}
+
+	activateSlide(slide) {
+		slide.classList.add('active');
+	}
+}
 
