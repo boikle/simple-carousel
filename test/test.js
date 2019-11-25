6f66e97e4e733ac3581f92b7ecdb175ba0e0842d
Robert Oikle <6431161+roikle@users.noreply.github.com>
2019-11-24 21:17:41 -0500

    Added a couple test cases for the slides class.

--- test/test.js
+++ test/test.js
@@ -1,8 +1,13 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
+const Slides = require('../src/slides');
+const assert = require('assert');
+describe('Slides', function() {
+	describe('getActiveSlide()', function() {
+		let slides = new Slides();
+		it('Should return a numeric value', function() {
+			assert.equal(typeof slides.getActiveSlide(), "number");
+		});
+		it('Should return an initial value of 0', function() {
+			assert.equal(slides.getActiveSlide(), 0);
+		});
+	});
 });
