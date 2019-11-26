4920756e845aeb15aeab4bda01bbddc887ee5315
boikle <boikle@users.noreply.github.com>
2019-11-26 18:42:09 -0500

    Added test for setActiveSlide method.

--- test/test.js
+++ test/test.js
@@ -1,13 +1,20 @@
 const Slides = require('../src/slides');
 const assert = require('assert');
 describe('Slides', function() {
+	let slides = new Slides();
 	describe('getActiveSlide()', function() {
-		let slides = new Slides();
 		it('Should return a numeric value', function() {
 			assert.equal(typeof slides.getActiveSlide(), "number");
 		});
 		it('Should return an initial value of 0', function() {
 			assert.equal(slides.getActiveSlide(), 0);
+		});
+	});
+
+	describe('setActiveSlide()', function() {
+		it('Should set active slide index to 1', function() {
+			slides.setActiveSlide(1);
+			assert.equal(slides.getActiveSlide(), 1);
 		});
 	});
 });
