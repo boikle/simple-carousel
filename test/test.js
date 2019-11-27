6141a4fd9251cc9d114bebf87d70c89b3ac4e438
boikle <boikle@users.noreply.github.com>
2019-11-26 20:09:52 -0500

    Added documentation and renaming methods

--- test/test.js
+++ test/test.js
@@ -4,17 +4,17 @@ describe('Slides', function() {
 	let slides = new Slides();
 	describe('getActiveSlide()', function() {
 		it('Should return a numeric value', function() {
-			assert.equal(typeof slides.getActiveSlide(), "number");
+			assert.equal(typeof slides.getActiveSlideIndex(), "number");
 		});
 		it('Should return an initial value of 0', function() {
-			assert.equal(slides.getActiveSlide(), 0);
+			assert.equal(slides.getActiveSlideIndex(), 0);
 		});
 	});
 
 	describe('setActiveSlide()', function() {
 		it('Should set active slide index to 1', function() {
-			slides.setActiveSlide(1);
-			assert.equal(slides.getActiveSlide(), 1);
+			slides.setActiveSlideIndex(1);
+			assert.equal(slides.getActiveSlideIndex(), 1);
 		});
 	});
 });
